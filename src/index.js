55f093e2e1d59f8ae1c01b442150dd08d5e95984
Maria Julieta Vignoli <Maria.Vignol@alu.frt.utn.edu.ar>
2025-05-18 20:36:54 -0300

    Configure mongoose

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 
+import './database/database.js';
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
