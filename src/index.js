cc21118e409ab6f91bf9202baf8b987701dcba88
Maria Julieta Vignoli <Maria.Vignol@alu.frt.utn.edu.ar>
2025-05-18 19:22:53 -0300

    Configuraciones iniciales del backend

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
